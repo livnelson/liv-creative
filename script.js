3c2be06991dc1910f00d131f1cb4d08f81ccdabf
Liv Nelson <livnelson@me.com>
2022-09-24 22:23:08 -0700

    Add basic HTML, CSS and JavaScript files

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Welcome to my website";
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
 
-document.querySelector("body").appendChild(h2);
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
